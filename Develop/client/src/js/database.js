83173d25dfbc20575ffc0a4af10e2195440126bc
Tzuzu <natoiodice@gmail.com>
2023-08-22 01:15:10 -0700

    Fixed database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,9 +15,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('content', 1);
-  const tx = todosDb.transaction('content', 'readwrite');
-  const store = tx.objectStore('content');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   const request = store.put({ id: id, content: content });
   const result = await request;
   console.log('Data saved to the database', result);
@@ -27,9 +27,9 @@ export const putDb = async (id, content) => {
 
 export const getDb = async () => {
   console.log('GET from the database');
-  const indexedDb = await openDB('index', 1);
-  const tx = indexedDb.transaction('index', 'readonly');
-  const store = tx.objectStore('index');
+  const indexedDb = await openDB('jate', 1);
+  const tx = indexedDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   console.log(`result.value`, result);
